9cf05e274b9aab01f771dc64d5339885a92094b6
David Alvarado <davidealva@gmail.com>
2017-01-17 12:40:13 -0700

    Fixed Show

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,38 +11,7 @@ Setting.create(
   linkedin_message: "A new album by David Alvarado and we're offering you an opportunity to get a Pre Release digital copy for free"
   )
 
-Prize.create(
-  name: "15% off Digital Release of \"Machines Can Talk\" Album",
-  number_of_referrals: 5,
-  image_file_name: nil,
-  image_content_type: nil,
-  image_file_size: nil,
-  image_updated_at: nil
- )
-
- Prize.create(
-   name: "Free Download on Bandcamp + 15% off \"Machines Can Talk\" Album",
-   number_of_referrals: 10,
-   image_file_name: nil,
-   image_content_type: nil,
-   image_file_size: nil,
-   image_updated_at: nil
- )
-
- Prize.create (
-   name: "Free Digital Download of \"Machines Can Talk\" Album",
-   number_of_referrals: 20,
-   image_file_name: nil,
-   image_content_type: nil,
-   image_file_size: nil,
-   image_updated_at: nil
- )
-
- Prize.create (
-   name: "Free Digital Copy of \"Machines Can Talk\" + Entry to Win Special Gift Packages",
-   number_of_referrals: 50,
-   image_file_name: nil,
-   image_content_type: nil,
-   image_file_size: nil,
-   image_updated_at: nil
- )
+Prize.create(name:'15% off Digital Release of "Machines Can Talk" Album', number_of_referrals: 5, image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil)
+Prize.create(name:'Free Download on Bandcamp + 15% off "Machines Can Talk" Album', number_of_referrals: 10, image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil)
+Prize.create(name:'Free Digital Download of "Machines Can Talk" Album', number_of_referrals: 20, image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil)
+Prize.create(name:'Free Digital Copy of "Machines Can Talk" + Entry to Win Special Gift Packages', number_of_referrals: 50, image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil)
